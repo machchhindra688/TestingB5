54e4e5abc50c9604a562a6f71660dcdbe0427d2f
Care <Care@DESKTOP-1PUJE8E>
2020-09-11 10:45:21 -0700

    Adding

--- git/src/com/cjc/git/Demo.java
+++ git/src/com/cjc/git/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello CJC");
+		System.out.println("Hello CJC1");
 		System.out.println("Hello Cjc Pune");
 		System.out.println("End class");
 	}
